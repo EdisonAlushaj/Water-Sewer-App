4e4d86f663773fceb97dcbac9e3a356197ec2a3c
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2024-07-26 16:00:17 +0200

    App Start
    Krijimi i React App dhe krijimi i Header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header/header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/header.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+
+function Header() {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ width: '100%', height: '100px'}}>
+                <div className="container-fluid" style={{ width: '100%' }}>
+                    <a className="navbar-brand" href="#">Water Sewer</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About Us</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact Us</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
