d5ecc2e0cae787875762dc7a98ff5340276c1f69
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2024-07-31 15:44:45 +0200

    Header Update
    Rregullimi i Header

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { MainLayout } from "./App.jsx";
+import Home from './Home/home.jsx'
 
 const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/app" />} />
-    <Route path="/app" element={<MainLayout />} />
-  </Routes>
+    <Routes>
+        <Route path="/" element={<Navigate to="/app" />} />
+        <Route path="/app" element={<MainLayout />}/>
+    </Routes>
 );
 
 export default AppRoutes;
--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -4,31 +4,31 @@ function Header() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-center" style={{ width: '100%', height: '100px', backgroundColor: ''}}>
-                <div className="container-fluid d-flex justify-content-center" style={{ width: '100%' }}>
-                    <a className="navbar-brand" href="#">Water Sewer</a>
+            <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-center" style={{ width: '100%', height: '100px', zIndex: '2', position: 'absolute'}}>
+                <div className="container-fluid d-flex justify-content-center" style={{ width: '100%'}}>
+                    <a className="navbar-brand" style={{ color: 'white'}} href="#">Water Sewer</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <div className="collapse navbar-collapse justify-content-center"  id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <li className="nav-item" >
+                                <a className="nav-link active" aria-current="page" href="#" style={{ color: 'white'}}>Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About Us</a>
+                                <a className="nav-link" href="#" style={{ color: 'white'}}>About Us</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Services</a>
+                                <a className="nav-link" href="#" style={{ color: 'white'}}>Services</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Projects</a>
+                                <a className="nav-link" href="#" style={{ color: 'white'}}>Projects</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Contact Us</a>
+                                <a className="nav-link" href="#" style={{ color: 'white'}}>Contact Us</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style={{ color: 'white'}}>Disabled</a>
                             </li>
                         </ul>
                     </div>
